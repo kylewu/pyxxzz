d21937efd31703ec62496873f0ec6ac24c1af6c1
Kyle Wu <imkylewu@gmail.com>
2011-03-17 22:30:30 +0100

    update

---
+++ test_littlewar.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+
+    Author:
+        Wenbin Wu <admin@wenbinwu.com>
+        http://www.wenbinwu.com
+ 
+    File:             test_littlewar.py
+    Create Date:      Thu Mar 17 22:29:33 2011
+
+'''
+
+def check_sig():
+    key = "9eeb28f7617b482ab001f67043b3e177"
+    keyName = "a548d6aefbeb32e0"
+    requestSig='fc5e065b5c9b3bc528fad54a195533ee'
+
+    lw = LittleWar()
+    print lw.get_sig(key, keyName)
+    if lw.get_sig(key, keyName) == requestSig:
+        print 'ok'
+
+def test_json():
+    f = open('attack_return')
+    str = f.read(999999)
+    j = json.loads(str)
+    print ['info']['enter_scene']['player_info']['pve']
+
+def check():
+    f1 = open('cc')
+    f1 = f1.read(999999)
+    f2 = open('dd')
+    f2 = f2.read(999999)
+    print len(f1), len(f2)
+    for i in range(len(f1)):
+        if f1[i] != f2[i]:
+            print i
+            print f1[i:]
+            break
+
+#test_json()
+#check()

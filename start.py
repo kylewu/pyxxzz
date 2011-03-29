34c2697976a2105c6a41a2ff787973ed9434f67a
Kyle Wu <imkylewu@gmail.com>
2011-03-29 16:47:02 +0200

    update

---
+++ start.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+
+    Author:
+        Wenbin Wu <admin@wenbinwu.com>
+        http://www.wenbinwu.com
+ 
+    File:             start.py
+    Create Date:      Thu Mar 17 10:25:11 2011
+
+'''
+import littlewar
+
+if __name__ == '__main__':
+    user_list = list()
+    password = ''
+
+    # Uncomment the following line
+    #littlewar.multiple_start(user_list, password, produce_id=1, logfile='log', loop=True)

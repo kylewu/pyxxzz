4520aa087e1a91fd716ee358d8d32567e0ad32f0
Kyle Wu <imkylewu@gmail.com>
2011-04-08 14:38:15 +0200

    update

--- start.py
+++ start.py
@@ -10,12 +10,13 @@
     Create Date:      Thu Mar 17 10:25:11 2011
 
 '''
-import time
 import littlewar
 
 if __name__ == '__main__':
     user_list = ['wenbin%s@wenbinwu.com' % n for n in ['10', '11', '12', '13', '14', '15']]
     user_list.append('wenbin@wenbinwu.com')
-    password = '1234'
+    #user_list = ['wenbin14@wenbinwu.com']
+    password = ''
+    my_ids = [59094425,355852754,356032671,356151199,356298870,356527009,356837352]
 
-    littlewar.multiple_start(user_list, password, id=2, logfile='log', loop=True)
+    littlewar.multiple_start(user_list, password, produce_id=1, logfile='log', loop=True)
